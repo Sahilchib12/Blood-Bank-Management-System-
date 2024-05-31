abf1e3d3f0f4491c0bc927302f295eb1be855ee4
Sahilchib12 <2021a1r160@mietjammu.in>
2024-05-31 13:44:23 +0530

    my project

---
+++ index.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blood Bank Management</title>
+    <!-- Bootstrap files -->
+    <link rel="stylesheet" href="bootstrap/css//bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="index.php">Blood Bank Management System</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="admin/login.php">Admin</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="donor/login.php">Donor</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="patient/login.php">Patient</a>
+            </li>
+            </ul>
+        </div>
+    </nav>
+<!-- Main content -->
+<div class="container-fluid" style="margin:50px;">
+    <div class="row container" style="text-align: justify;">
+        <div class="col-md-8 mx-auto content-left">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>A Blood Management System (BMS) is a comprehensive framework that encompasses all processes and activities involved in the collection, testing, processing, storage, and distribution of blood and blood products. This system ensures that blood and its components are available, safe, and used effectively to meet the needs of patients. Here are the key components and functions of a blood management system
+                
+            </p>
+            <button class="btn btn-danger">Read more..</button>
+        </div>
+        <div class="col-md-3 content-right">
+            <img class="img-fluid" src="../BBMS/images/image1.jpg" height="auto" width="70%">
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-10 m-auto" style="text-align: justify;">
+            <h4>What is Blood Bank Management System?</h4>
+            <p>
+            Blood Donation and Collection:
+
+Organizing blood donation drives.
+Recruiting and retaining donors.
+Collecting blood from voluntary, non-remunerated donors.
+Testing and Screening:
+
+Screening donated blood for infectious diseases (e.g., HIV, hepatitis B and C, syphilis).
+Blood typing and cross-matching to ensure compatibility.
+Processing:
+
+Separating whole blood into components such as red blood cells, plasma, platelets, and cryoprecipitate.
+Storing blood components under appropriate conditions.
+Inventory Management:
+
+Tracking blood inventory to ensure a sufficient supply.
+Implementing a first-in, first-out (FIFO) system to use older blood products first.
+Distribution and Transportation:
+
+Coordinating the distribution of blood products to hospitals and clinics.
+Ensuring safe and timely transportation under controlled conditions.
+            <p>
+            Monitoring blood usage and transfusion practices.
+Implementing guidelines to ensure appropriate use of blood products.
+Managing blood transfusion reactions and complications.
+Quality Assurance and Compliance:
+
+Ensuring compliance with national and international standards and regulations.
+Conducting regular audits and quality control checks.
+Implementing corrective actions and continuous improvement practices.
+Information Management:
+
+Utilizing blood management software systems for record-keeping and data management.
+Ensuring traceability from donor to recipient.
+Analyzing data to improve efficiency and outcomes
+            </p>
+        </div>
+    </div>
+</div>
+<div class="container-fluid" style="margin-bottom: 5%;">
+    <div class="row">
+        <div class="col-10  m-auto">
+        <h4>Our centers?</h4>
+        <div class="card container">
+            <h4>Jaipur</h4>
+            <hr>OK
+            <p>
+        </div>
+        <div class="card">
+            <h4>Delhi</h4>
+            <hr>ok
+            <>
+       </div>
+        <div class="card">
+            <h4>Patna</h4>
+            <hr>ok
+            <>
+       </div>
+        <div class="card">
+            <h4>Mumbai</h4>
+            <hr>ok
+            <> 
+            <p>
+       </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12 bg-danger footer">
+            &copy 2024 CSE MINI PROJECT
+        </div>
+    </div>
+</div>
+</body>
+</html>
